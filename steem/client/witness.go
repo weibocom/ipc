e8b0a42c02f9d141ef8638b6c124f2edb515cb31
smallnest <smallnest@gmail.com>
2018-05-08 17:12:37 +0800

    use the right maximumBlockSize

--- steem/client/witness.go
+++ steem/client/witness.go
@@ -4,7 +4,21 @@ import (
 	"github.com/weibocom/ipc/steem/types"
 )
 
+var maximumBlockSize uint32
+
+func (c *Client) getMaximumBlockSize() (uint32, error) {
+	props, err := c.Database.GetDynamicGlobalProperties()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint32(props.MaximumBlockSize.Int64()), nil
+}
 func (c *Client) AddWitness(privateKeys [][]byte, owner string, blockSigningKey string, url string, fee int64) error {
+
+	if maximumBlockSize == 0 {
+		maximumBlockSize, _ = c.getMaximumBlockSize()
+	}
 	operation := &types.WitnessUpdateOperation{
 		Owner:           owner,
 		Fee:             types.NewSteemAsset(fee),
@@ -12,7 +26,7 @@ func (c *Client) AddWitness(privateKeys [][]byte, owner string, blockSigningKey 
 		BlockSigningKey: types.PublicKey(blockSigningKey),
 		Props: &types.ChainProperties{
 			AccountCreationFee: types.NewSteemAsset(0),
-			MaximumBlockSize:   13107200,
+			MaximumBlockSize:   maximumBlockSize,
 			SBDInterestRate:    0,
 		},
 	}
