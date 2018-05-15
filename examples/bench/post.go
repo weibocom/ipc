79c9fbfc8a4f8bda2260cfa1b3c0cf4df52a485a
smallnest <smallnest@gmail.com>
2018-05-15 17:33:44 +0800

    add bench code

--- examples/bench/post.go
+++ examples/bench/post.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"runtime"
 	"strconv"
 	"time"
 
-	"github.com/weibocom/ipc/keys"
-
 	"github.com/juju/ratelimit"
-	"github.com/weibocom/ipc/steem/client"
-	"github.com/weibocom/ipc/steem/types"
+	"github.com/weibocom/ipc/client"
+	"github.com/weibocom/ipc/store"
 	"github.com/weibocom/ipc/transports/websocket"
 
 	metrics "github.com/rcrowley/go-metrics"
 )
 
 var (
-	rpcServer = flag.String("rpc", "ws://52.80.76.2:8090", "steem rpc server")
+	rpcServer = flag.String("rpc", "ws://52.80.76.2:48090", "steem rpc server")
+	n         = flag.Int("n", 5000, "user count")
+	initUser  = flag.Bool("user", false, "init users")
 	batch     = flag.Int("batch", 1, "operation count in one batch")
 	cc        = flag.Int("c", runtime.GOMAXPROCS(-1), "concurrency count")
 	pr        = flag.Int("pr", 2000, "max rate for post, not used yet")
@@ -33,6 +34,20 @@ var (
 
 func main() {
 	flag.Parse()
+	if *initUser {
+		tran, err := websocket.NewTransport([]string{*rpcServer})
+		if err != nil {
+			log.Fatalf("failed to new transport:%s", err.Error())
+		}
+		defer tran.Close()
+		c, cerr := client.NewClient(tran, store.NewMemStore("weibo"))
+		if cerr != nil {
+			log.Fatalf("failed to new client:%s", cerr.Error())
+		}
+
+		initAccounts(c, *n)
+		c.Close()
+	}
 
 	go metrics.Log(reg, 10*time.Second, log.New(os.Stdout, "metrics: ", log.Lmicroseconds))
 	postLimter := ratelimit.NewBucketWithQuantum(time.Second, int64(*pr), int64(*pr))
@@ -46,25 +61,19 @@ func main() {
 				log.Fatalf("failed to new transport:%s", err.Error())
 			}
 			defer tran.Close()
-			c, cerr := client.NewClient(tran)
+			c, cerr := client.NewClient(tran, store.NewMemStore("weibo"))
 			if cerr != nil {
 				log.Fatalf("failed to new client:%s", cerr.Error())
-				os.Exit(-2)
 			}
 			defer c.Close()
 
 			for j := 0; ; j++ {
 				postLimter.Wait(1)
 				start := time.Now()
-
-				var ops []types.Operation
-				for k := 0; k < *batch; k++ {
-					op := client.CreateCommentOperation("initminer", "人民日报评论：如何聆听“年轻的声音”？​​​"+strconv.Itoa(i)+strconv.Itoa(j)+strconv.Itoa(k), "这几天，一封来自北大学生的公开信传播甚广。信中提到的北大相关学院对这位同学提请信息公开一事的处置，引发舆论关注和思考。", "weibo-8000-9000", "wb", "", []string{"test"})
-					ops = append(ops, op)
-
-				}
-				_, err = c.BatchPost(keys.GetPrivateKeys(), ops)
-
+				jj := j % *n
+				_, err := c.Post("wb-"+strconv.Itoa(jj), "人民日报评论：如何聆听“年轻的声音”？​​​"+strconv.Itoa(i)+strconv.Itoa(j),
+					[]byte("这几天，一封来自北大学生的公开信传播甚广。信中提到的北大相关学院对这位同学提请信息公开一事的处置，引发舆论关注和思考。"),
+					strconv.Itoa(i)+strconv.Itoa(j), []string{"test"})
 				if err == nil {
 					postSuccessMeter.UpdateSince(start)
 				} else {
@@ -76,4 +85,19 @@ func main() {
 	}
 
 	select {}
+}
+
+func initAccounts(c client.Client, n int) {
+	fmt.Println("start to init users")
+	var err error
+	for i := 1; i <= n; i++ {
+		_, err = c.CreateAccount("wb-"+strconv.Itoa(i), `{"meta":"data"}`)
+		if err != nil {
+			fmt.Printf("create account:%v, %v\n", "wb-"+strconv.Itoa(i), err)
+		} else {
+			fmt.Printf("create account:%v succeeded\n", "wb-"+strconv.Itoa(i))
+		}
+	}
+
+	fmt.Println("finished to init users")
 }
